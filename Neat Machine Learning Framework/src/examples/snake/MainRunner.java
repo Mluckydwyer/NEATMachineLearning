540be47e1f183450eb99c03186485174325beb70
Ben <Ben@208.110.230.38>
2017-08-20 17:17:32 -0500

    hope

---
+++ Neat Machine Learning Framework/src/examples/snake/MainRunner.java
@@ -0,0 +1,11 @@
+package examples.snake;
+import javax.swing.JApplet;
+public class MainRunner {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+NeatSnake neatsnake = new NeatSnake();
+//runs
+	}
+
+}
--- Neat Machine Learning Framework/src/examples/snake/NeatSnake.java
+++ Neat Machine Learning Framework/src/examples/snake/NeatSnake.java
@@ -1,6 +1,7 @@
 package examples.snake;
 
 import java.awt.Color;
+
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -10,33 +11,35 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.swing.JApplet;
+import javax.swing.JFrame;
 import javax.swing.Timer;
 
 import core.Neat;
 import core.Objective;
 import core.Species;
 
-public class NeatSnake extends JApplet implements Objective, KeyListener {
+public class NeatSnake implements Objective, KeyListener {
 
 	private static final long serialVersionUID = 8208701794491690571L;
 	public enum Direction {
 		UP, DOWN, LEFT, RIGHT
 	}
-	
+	//neat
 	private Neat neat;
 	private Random random;
 	private ArrayList<Snake> snakes;
-	private int[][][] gameBoards; // snake #, x, y | 0 = empty, -1 = food, 1-... = snake parts in numbered order
 	public static final int numSquares = 15;
 	private final int squareSize = 50;
 	private boolean AI = true; // Toggles Neat Genetic Learning Framework
 	private boolean running;
 	private Point center;
+	private JFrame mainFrame;
+	Graphics graphic;
 
 	public NeatSnake() {		
 		snakes = new ArrayList<>();
-		
+		mainFrame=new JFrame();
+		graphic = mainFrame.getGraphics();
 		if (AI) neat = new Neat(this);
 		else snakes.add(new Snake());
 
@@ -45,10 +48,9 @@ public class NeatSnake extends JApplet implements Objective, KeyListener {
 		running = true;
 		center = new Point((int) Math.ceil(numSquares / 2), (int) Math.ceil(numSquares / 2));
 		
-		
-		setSize(numSquares * squareSize, numSquares * squareSize); // 15 x 15 squares each 50 pixels
-		setBackground(Color.gray);
-
+		mainFrame.setVisible(true);
+		mainFrame.setSize(numSquares * squareSize, numSquares * squareSize); // 15 x 15 squares each 50 pixels
+		mainFrame.getContentPane().setBackground(Color.WHITE);
 		Timer t = new Timer(1000 / 60, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -77,11 +79,18 @@ public class NeatSnake extends JApplet implements Objective, KeyListener {
 	}
 
 	private void draw() {
-		Graphics g = getGraphics();
-
+		for(Snake a:snakes){
+			for(int i=0;i<numSquares;i++){
+				for(int j=0;j<numSquares;j++){
+					
+					graphic.setColor(Color.BLACK);
+					graphic.fillRect(i*squareSize,j*squareSize,squareSize,squareSize);
+				}
+			}
+		}
 		for (int i = squareSize - 1; i < numSquares; i += squareSize) {
-			g.drawLine(i, 0, i, getHeight());
-			g.drawLine(0, i, getWidth(), i);
+			graphic.drawLine(i, 0, i, mainFrame.getHeight());
+			graphic.drawLine(0, i, mainFrame.getWidth(), i);
 		}
 	}
 
